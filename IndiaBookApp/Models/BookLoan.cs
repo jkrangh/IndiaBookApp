7217113577a54ccc9d2d5c46b2ca683274b9dc30
Johan <147413295+jkrangh@users.noreply.github.com>
2024-06-05 11:31:30 +0200

    Added models

---
+++ IndiaBookApp/Models/BookLoan.cs
@@ -0,0 +1,11 @@
+﻿namespace IndiaBookApp.Models
+{
+    public class BookLoan
+    {
+        public int Id { get; set; }
+        public Book Book { get; set; }
+        public User User { get; set; }
+        public DateTime LoanDate { get; set; } = DateTime.Now;
+        public DateTime LoanExpires { get; set; }
+    }
+}
