b31f71189fb3f77b3dad60807362b09dc447815d
Johan <147413295+jkrangh@users.noreply.github.com>
2024-06-05 13:55:09 +0200

    Seed book data

--- IndiaBookApp/Data/ApplicationDbContext.cs
+++ IndiaBookApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using IndiaBookApp.Models;
+using Newtonsoft.Json;
 
 namespace IndiaBookApp.Data
 {
@@ -11,5 +12,21 @@ namespace IndiaBookApp.Data
         {
         }
         public DbSet<IndiaBookApp.Models.Book> Books { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(SeedBookData());
+        }
+
+        public List<Book> SeedBookData()
+        {
+            var books = new List<Book>();
+            using (StreamReader r = new StreamReader(@"wwwroot/seed-data/books.json"))
+            {
+                string json = r.ReadToEnd();
+                books = JsonConvert.DeserializeObject<List<Book>>(json);
+            }
+            return books;
+        }
     }
 }
--- IndiaBookApp/Data/Migrations/20240605113926_Initial.Designer.cs
+++ IndiaBookApp/Data/Migrations/20240605113926_Initial.Designer.cs
@@ -4,6 +4,7 @@ using IndiaBookApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IndiaBookApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605113926_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
